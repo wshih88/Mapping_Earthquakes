8f1eae19075f2e4fa47cdd54d3137906fd67e666
winston.shih88 <winston.shih88@gmail.com>
2020-06-02 23:45:39 -0700

    updating multiple geojson

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -1,38 +1,8 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
-// Create the map object with center at the San Francisco airport.
-let map = L.map('mapid').setView([37.5, -122.5], 10);
-
-// Coordinates for each point to be used in the line.
-let line = [
-	[33.9416, -118.4085],
-	[37.6213, -122.3790],
-	[40.7899, -111.9791],
-	[47.4502, -122.3088]
-  ];
-
-// Create a polyline using the line coordinates and make the line black.
-L.polyline(line, {
-	color: "yellow"
- }).addTo(map);
-
-
-// Get data from cities.js
-let cityData = cities;
-
-// Loop through the cities array and create one marker for each city.
-cityData.forEach(function(city) {
-	console.log(city)
-	L.circleMarker(city.location, {
-		radius: (city.population-200000)/100000
-	})
-	.bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
-	.addTo(map);
-	//console.log(city.population/100000)
-	//console.log((city.population-200000)/100000);
-
-});
+// Create the map object with center and zoom level.
+let map = L.map('mapid').setView([30, 30], 2);
 
 
 // We create the tile layer that will be the background of our map.
@@ -45,6 +15,17 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 });
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
+
+// Accessing the airport GeoJSON URL
+let airportData = "https://raw.githubusercontent.com/wshih88/Mapping_Earthquakes/master/majorAirports.json";
+
+// Grabbing our GeoJSON data.
+d3.json(airportData).then(function(data) {
+  console.log(data);
+// Creating a GeoJSON layer with the retrieved data.
+L.geoJson(data).addTo(map);
+});
+
 
 // let streets = L.tileLayer(' https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 // attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
