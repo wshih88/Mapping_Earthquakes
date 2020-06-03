fefae7db3a3c277c4799a9e7a17c8cec0321d8da
winston.shih88 <winston.shih88@gmail.com>
2020-06-02 19:13:39 -0700

    adding multiple cities with multiple lines

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -1,8 +1,22 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
-// Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([40.7, -94.5], 4);
+// Create the map object with center at the San Francisco airport.
+let map = L.map('mapid').setView([37.6213, -122.3790], 5);
+
+// Coordinates for each point to be used in the line.
+let line = [
+	[33.9416, -118.4085],
+	[37.6213, -122.3790],
+	[40.7899, -111.9791],
+	[47.4502, -122.3088]
+  ];
+
+// Create a polyline using the line coordinates and make the line black.
+L.polyline(line, {
+	color: "yellow"
+ }).addTo(map);
+
 
 // Get data from cities.js
 let cityData = cities;
@@ -15,8 +29,8 @@ cityData.forEach(function(city) {
 	})
 	.bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
 	.addTo(map);
-	console.log(city.population/100000)
-	console.log((city.population-200000)/100000);
+	//console.log(city.population/100000)
+	//console.log((city.population-200000)/100000);
 
 });
 
